e45a40bd34e60f489ffcc64797bf3f5d3b79bcbd
Josh Caulfield <Coolfield@users.noreply.github.com>
2020-05-09 17:41:12 +0100

    Update and rename Udacity/adjust_results4_isadog.py to Udacity/pre_trained/adjust_results4_isadog.py

--- Udacity/pre_trained/adjust_results4_isadog.py
+++ Udacity/pre_trained/adjust_results4_isadog.py
@@ -67,4 +67,37 @@ def adjust_results4_isadog(results_dic, dogfile):
     Returns:
            None - results_dic is mutable data type so no return needed.
     """           
-    None
+    
+    dognames_dic = dict()
+
+    with open(dogfile, "r") as infile:
+        line = infile.readline()
+
+        while line != "":
+
+            stripped_line = line.strip('\n')
+
+            if stripped_line not in dognames_dic:
+                dognames_dic[stripped_line] = 1
+
+            line = infile.readline()
+    
+    for key in results_dic:
+
+        if results_dic[key][0] in dognames_dic:
+            
+            if results_dic[key][1] in dognames_dic:
+                results_dic[key].extend((1, 1))
+
+            else:
+                results_dic[key].extend((1,0))
+
+        else:
+
+            if results_dic[key][1] in dognames_dic:
+                results_dic[key].extend((0,1))
+
+            else:
+                results_dic[key].extend((0,0))
+                
+
